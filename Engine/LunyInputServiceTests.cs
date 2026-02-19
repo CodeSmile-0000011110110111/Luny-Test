f2fb3984524f75e3f7307d1714c42cf54827d1e7
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-02-19 14:10:45 +0100

    separate axis input, add input logging

--- Engine/LunyInputServiceTests.cs
+++ Engine/LunyInputServiceTests.cs
@@ -10,11 +10,6 @@ namespace Luny.Test.Engine
 	/// </summary>
 	internal sealed class TestInputService : LunyInputServiceBase, ILunyInputService
 	{
-		public void SimulateAxisInput(String actionName, LunyVector2 value) => RaiseDirectionalInput(actionName, value);
-
-		public void SimulateButtonInput(String actionName, Boolean pressed, Single analogValue = 1f) =>
-			RaiseButtonInput(actionName, pressed, analogValue);
-
 		public void SimulatePostUpdate() => OnServicePostUpdate();
 	}
 
@@ -28,25 +23,25 @@ namespace Luny.Test.Engine
 
 		[Test]
 		public void GetAxisValue_Unknown_Action_Returns_Default() =>
-			Assert.That(_service.GetAxisValue("Unknown"), Is.EqualTo(default(LunyVector2)));
+			Assert.That(_service.GetDirection("Unknown"), Is.EqualTo(default(LunyVector2)));
 
 		[Test]
 		public void GetAxisValue_Returns_Last_Known_Value()
 		{
 			var vec = new LunyVector2(0.5f, -0.3f);
-			_service.SimulateAxisInput("Move", vec);
+			_service.SimulateDirectionalInput("Move", vec);
 
-			Assert.That(_service.GetAxisValue("Move"), Is.EqualTo(vec));
+			Assert.That(_service.GetDirection("Move"), Is.EqualTo(vec));
 		}
 
 		[Test]
 		public void GetAxisValue_NotCleared_On_PostUpdate()
 		{
 			var vec = new LunyVector2(1f, 0f);
-			_service.SimulateAxisInput("Move", vec);
+			_service.SimulateDirectionalInput("Move", vec);
 			_service.SimulatePostUpdate();
 
-			Assert.That(_service.GetAxisValue("Move"), Is.EqualTo(vec));
+			Assert.That(_service.GetDirection("Move"), Is.EqualTo(vec));
 		}
 
 		[Test]
@@ -108,24 +103,24 @@ namespace Luny.Test.Engine
 		}
 
 		[Test]
-		public void GetButtonValue_Returns_Analog_Value()
+		public void GetButtonStrength_Returns_Analog_Value()
 		{
 			_service.SimulateButtonInput("Trigger", true, 0.75f);
 
-			Assert.That(_service.GetButtonValue("Trigger"), Is.EqualTo(0.75f));
+			Assert.That(_service.GetButtonStrength("Trigger"), Is.EqualTo(0.75f));
 		}
 
 		[Test]
-		public void GetButtonValue_Returns_Zero_After_Release()
+		public void GetButtonStrength_Returns_Zero_After_Release()
 		{
 			_service.SimulateButtonInput("Trigger", true, 0.75f);
 			_service.SimulateButtonInput("Trigger", false);
 
-			Assert.That(_service.GetButtonValue("Trigger"), Is.EqualTo(0f));
+			Assert.That(_service.GetButtonStrength("Trigger"), Is.EqualTo(0f));
 		}
 
 		[Test]
-		public void GetButtonValue_Unknown_Action_Returns_Zero() => Assert.That(_service.GetButtonValue("Unknown"), Is.EqualTo(0f));
+		public void GetButtonValue_Unknown_Action_Returns_Zero() => Assert.That(_service.GetAxis("Unknown"), Is.EqualTo(0f));
 
 		[Test]
 		public void OnInputAction_Event_Fires_For_Axis()
@@ -133,11 +128,11 @@ namespace Luny.Test.Engine
 			LunyInputEvent received = default;
 			_service.OnInputAction += e => received = e;
 
-			_service.SimulateAxisInput("Move", new LunyVector2(1f, 0f));
+			_service.SimulateDirectionalInput("Move", new LunyVector2(1f, 0f));
 
 			Assert.That(received.ActionName, Is.EqualTo("Move"));
-			Assert.That(received.ActionType, Is.EqualTo(LunyInputActionType.Axis));
-			Assert.That(received.AxisValue, Is.EqualTo(new LunyVector2(1f, 0f)));
+			Assert.That(received.ActionType, Is.EqualTo(LunyInputActionType.Directional));
+			Assert.That(received.Direction, Is.EqualTo(new LunyVector2(1f, 0f)));
 		}
 
 		[Test]
